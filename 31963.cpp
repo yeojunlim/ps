3fb6e9038baefe5cf17c700c43dc27f5a5136479
yeojunlim <kr.yeojun@gmail.com>
2025-11-30 14:52:50 +0900

    31963

--- 31963.cpp
+++ 31963.cpp
@@ -1,23 +1,22 @@
-#include <cstdio>
-#include <algorithm>
-#include <cmath>
-#include <numeric>
-#define MAXN 250001
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
 int N;
-ll A[MAXN],rst[MAXN]={0LL};
+vector<ll> A, rst;
+int a;
 
 int main()
 {
-    scanf("%d",&N);
-    for (int i=0;i<N;i++) scanf("%lld",&A[i]);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> N;
     
-    for (int i=1;i<N;i++) {
-        rst[i]=max(0LL, (ll)ceil(log2((double)A[i-1]/A[i]))+rst[i-1]);
-    }
+    for (int i = 0; i < N; i++) A.push_back((cin >> a, a));
+    rst.push_back(0);
 
-    printf("%lld\n",accumulate(rst,rst+N,0LL));
+    for (int i = 1; i < N; i++) rst.push_back(max(0LL, (ll)ceil(log2((double)A[i-1] / A[i])) + rst[i-1]));
+    cout << accumulate(rst.begin(), rst.end(), 0LL) << '\n';
+    
     return 0;
 }
